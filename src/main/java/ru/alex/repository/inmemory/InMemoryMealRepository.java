4e36fc61cab5bbbcb1a48edeaacb247feafe9801
Alexey Tarabanov <tarabanov@bk.ru>
2021-11-05 20:42:47 +0300

    add_postConstruct_and_preDestroy

--- src/main/java/ru/alex/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/alex/repository/inmemory/InMemoryMealRepository.java
@@ -1,11 +1,15 @@
 package ru.alex.repository.inmemory;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import ru.alex.model.Meal;
 import ru.alex.repository.MealRepository;
 import ru.alex.util.UserMealsUtil;
 import ru.alex.util.Util;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.Collections;
@@ -17,6 +21,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+import static java.lang.System.getLogger;
 import static ru.alex.repository.inmemory.InMemoryUserRepository.ADMIN_ID;
 import static ru.alex.repository.inmemory.InMemoryUserRepository.USER_ID;
 
@@ -30,6 +35,8 @@ import static ru.alex.repository.inmemory.InMemoryUserRepository.USER_ID;
 @Repository
 public class InMemoryMealRepository implements MealRepository {
 
+    private static final Logger log = LoggerFactory.getLogger(InMemoryMealRepository.class);
+
     private final Map<Integer, InMemoryBaseRepository<Meal>> usersMealsMap = new ConcurrentHashMap<>();
     private AtomicInteger counter = new AtomicInteger(0);
 
@@ -37,7 +44,7 @@ public class InMemoryMealRepository implements MealRepository {
         UserMealsUtil.meals.forEach(meal -> save(meal, USER_ID));
         save(new Meal(LocalDateTime.of(2021, Month.OCTOBER, 29, 14, 0), "Админ ланч", 510), ADMIN_ID);
         save(new Meal(LocalDateTime.of(2021, Month.OCTOBER, 29, 21, 0), "Админ ужин", 1500), ADMIN_ID);
-        save(new Meal(LocalDateTime.of(2021, Month.NOVEMBER, 04, 12, 04), "Админ попил чаек", 100), ADMIN_ID);
+        save(new Meal(LocalDateTime.of(2021, Month.NOVEMBER, 4, 12, 4), "Админ попил чаек", 100), ADMIN_ID);
     }
 
     @Override
@@ -47,6 +54,16 @@ public class InMemoryMealRepository implements MealRepository {
         InMemoryBaseRepository<Meal> meals = usersMealsMap.computeIfAbsent(userId, uid -> new InMemoryBaseRepository<>());
         return meals.save(meal);
     }
+
+    @PostConstruct
+    // Если появилась необходимость добавить какой-то код после создания bean'а,
+    // воспользуйтесь аннотацией @PostConstruct.
+    public void postConstruct() { log.info("+++ PostConstruct"); }
+
+    @PreDestroy
+    // Если необходимо выполнить какие-то действия перед уничтожением bean'а,
+    // воспользуйтесь аннотацией @PreDestroy.
+    public void preDestroy() { log.info("+++ PreDestroy"); }
 
     // теперь все методы будут начинаться с метода get()
     // получаем мапу еды для конкретного юзера
